f72ddafbd94324f514f19f6a302a169530f027b7
jibesh <jibeshshrestha00@gmail.com>
2025-02-14 14:52:08 +0545

    add license controller

--- Controllers/LicenseController.cs
+++ Controllers/LicenseController.cs
@@ -9,14 +9,14 @@ public class LicenseController : Controller
 {
     
     [HttpGet]
-    public IActionResult SelectVehicle()
+    public IActionResult Index()
     {
-        var vehicle1 = new Vehicle() { name = "something", code = "A" };
-        var vehicle2 = new Vehicle() { name = "something", code = "B" };
-        Console.WriteLine(vehicle1.name);
-        Console.WriteLine(vehicle2.name);
+        var vehicle1 = new Vehicle() { name = "bike", code = "A", imageUrl = "/images/bycicle.png", isSelected = false};
+        var vehicle2 = new Vehicle() { name = "scooter", code = "B", imageUrl = "/images/motorcycle.png", isSelected = false};
+        var vehicle3 = new Vehicle() { name = "car", code = "A", imageUrl = "/images/car.png", isSelected = false};
+        var vehicle4 = new Vehicle() { name = "truck", code = "A", imageUrl = "/images/truck.png", isSelected = false};
         
-        var Vehicles = new List<Vehicle>() { vehicle1, vehicle2 };
+        var Vehicles = new List<Vehicle>() { vehicle1, vehicle2, vehicle3, vehicle4 };
 
         foreach (var vehicle in Vehicles)
         {
@@ -24,6 +24,28 @@ public class LicenseController : Controller
         }
         
         return View(Vehicles);
+    }
+
+    [HttpPost]
+    [Route("License/SelectVehicle")]
+    public IActionResult SelectVehicle(string selectedCategory)
+    {
+        if (!string.IsNullOrEmpty(selectedCategory))
+        {
+            HttpContext.Session.SetString("selectedVehicle", selectedCategory);
+            return RedirectToAction("NextStep");
+        }
+        
+        TempData["ErrorMessage"] = "Please select a category before proceeding.";
+        return RedirectToAction("Index", "License");
+    }
+    
+    [HttpGet]
+    public IActionResult NextStep()
+    {
+        var selectedCategory = HttpContext.Session.GetString("selectedVehicle");
+        ViewBag.SelectedCategory = selectedCategory;
+        return View();
     }
     
     
